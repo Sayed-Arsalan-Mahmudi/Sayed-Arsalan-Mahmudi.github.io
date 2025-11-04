42ea8ea18cdcc93c3431265e0bbbb9d44c349f50
Sayed-Arsalan-Mahmudi <mahmudiarsalan0@gmail.com>
2025-11-04 20:29:20 +0300

    Website's templates has been uploaded.

---
+++ script.js
@@ -0,0 +1,16 @@
+const themeButton = document.getElementById("themeButton");
+// const wathThisVideoButton = document.getElementById("wathThisVideoButton");
+// const homePageButton = document.getElementById("homePageButton");
+const block = document.getElementById("block");
+
+
+themeButton.addEventListener('click', function() { // typing codes for one tag.
+    document.body.classList.toggle('dark'); // changes the body's css to .dark css if it's not .dark css.
+    themeButton.classList.toggle('darkButton'); // changes themeButton's css to .darkButton css if it's not .darkButton css.
+    if (themeButton.textContent === 'Dark') { // changes the text of the button.(NOT this line. The if else do it.)
+        themeButton.textContent = 'Light';
+        document.div.classList.toggle('darkDiv');
+    } else if (themeButton.textContent === 'Light') {
+        themeButton.textContent = 'Dark';
+    }
+});
